bd35ae8922ec013c423db978f3dd5d54414bff17
ranepaarth <2019paarth.rane@ves.ac.in>
2024-05-14 19:45:30 +0530

    feat: banner carousel
    Utilized react-responsive-carousel package to implement the banner carousel at the background of the home page. It comes with some additional properties to enhance the carousel experience.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,38 @@
-import Header from "@/components/header";
-import React from "react";
+"use client";
+
+import BannerCarousel from "@/components/banner/banner-carousel";
+import Header from "@/components/header/header";
+import React, { useEffect, useState } from "react";
 
 const Home = () => {
+  const [showOverlay, setShowOverlay] = useState(false);
+
+  const handleShowOverLay = () => {
+    console.log("scroll");
+    setShowOverlay((prev) => !prev);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      setShowOverlay(false);
+    });
+
+    return () => window.removeEventListener("scroll", () => {});
+  }, [showOverlay]);
+
   return (
-    <main>
-      <Header />
-    </main>
+    <div className="bg-neutral-100">
+      <Header showOverlay={handleShowOverLay} />
+      {showOverlay && (
+        <div
+          className="h-full z-50 absolute w-full bg-black opacity-50"
+          onClick={handleShowOverLay}
+        />
+      )}
+      <main className="max-w-screen-2xl mx-auto">
+        <BannerCarousel />
+      </main>
+    </div>
   );
 };
 
---
+++ src/components/banner/banner-carousel.tsx
@@ -0,0 +1,47 @@
+import Image from "next/image";
+import React from "react";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
+const banners = [
+  {
+    id: 1,
+    src: "/banner-1.jpg",
+    alt: "Shop your favorites in clothing, beauty & more",
+  },
+  { id: 2, src: "/banner-2.jpg", alt: "Tops & dresses" },
+  { id: 3, src: "/banner-3.jpg", alt: "Daily needs" },
+  { id: 4, src: "/banner-4.jpg", alt: "Exciting launches from top brands" },
+  { id: 5, src: "/banner-5.jpg", alt: "Smartphones" },
+];
+
+const BannerCarousel = () => {
+  return (
+    <div className="relative">
+      <div className="h-32 absolute bottom-0 bg-gradient-to-t from-neutral-100 z-20 w-full" />
+      <Carousel
+        autoPlay
+        infiniteLoop
+        interval={3000}
+        showIndicators={false}
+        showThumbs={false}
+        showStatus={false}
+      >
+        {banners.map((banner) => (
+          <div key={banner.id}>
+            <Image
+              src={banner.src}
+              alt={banner.alt}
+              width={1020}
+              height={720}
+              priority
+              quality={80}
+            />
+          </div>
+        ))}
+      </Carousel>
+    </div>
+  );
+};
+
+export default BannerCarousel;
