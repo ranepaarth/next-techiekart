fcfc150c46f31882bf9227260439bc5a6e0e5d62
ranepaarth <2019paarth.rane@ves.ac.in>
2024-05-15 18:54:57 +0530

    feat: 🚧 products feed
    Add loading state and UI to the products page to show up while the data is being fetched.

--- src/components/products-feed/products-feed.tsx
+++ src/components/products-feed/products-feed.tsx
@@ -19,15 +19,18 @@ const ProductsFeed = () => {
   }, []);
 
   if (isPending) {
-    return <div className="w-full text-slate-700 text-2xl flex justify-center"><BiLoaderAlt className="animate-spin" strokeWidth={2.5} /></div>;
+    return (
+      <div className="w-full text-slate-700 text-2xl flex justify-center">
+        <BiLoaderAlt className="animate-spin" strokeWidth={2.5} />
+      </div>
+    );
   }
 
   return (
     <section className="grid grid-cols-1  md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-20 lg:-mt-32 xl:-mt-52 grid-flow-row-dense mx-auto">
-      {data &&
-        data
-          .slice(0, 4)
-          .map((product) => <Product product={product} key={product.id} />)}
+      {data?.slice(0, 4).map((product) => (
+        <Product product={product} key={product.id} />
+      ))}
 
       <div className="col-span-full">
         <Image
